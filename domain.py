fa45ef20b7500b2498a1714bfe2f71e84b490e1a
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2023-02-12 15:12:15 +0100

    add function to get only domain name

---
+++ domain.py
@@ -0,0 +1,18 @@
+from urllib.parse import urlparse
+
+
+# Get subdomain name (names.example.com)
+def get_subdomain_name(url):
+    try:
+        return urlparse(url).netloc
+    except:
+        return ""
+
+
+# Get domain name (example.com)
+def get_domain_name(url):
+    try:
+        results = get_subdomain_name(url).split(".")
+        return f"{results[-2]}.{results[-1]}"
+    except:
+        return ""
